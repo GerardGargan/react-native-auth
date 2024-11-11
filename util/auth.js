17aecbe6946af40fa6482a34494a80cd8a2acbfd
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-11-11 22:18:37 +0000

    setup login and logout auth

--- util/auth.js
+++ util/auth.js
@@ -1,13 +1,26 @@
 import axios from "axios";
 
 const API_KEY = process.env.EXPO_PUBLIC_FIREBASE_KEY;
+const BASE_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:';
 
-export async function createUser(email, password) {
-    const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
+async function authenticate(mode, email, password) {
+    const url = `${BASE_URL}${mode}?key=${API_KEY}`;
+
+    const response = await axios.post(url, 
         {
-            email,
-            password,
-            returnSecureToken: true
-        }
-    );
+        email,
+        password,
+        returnSecureToken: true
+    });
+
+    const token = response.data.idToken;
+    return token;
+}
+
+export function createUser(email, password) {
+    return authenticate('signUp', email, password);
+}
+
+export function login(email, password) {
+    return authenticate('signInWithPassword', email, password);
 }
